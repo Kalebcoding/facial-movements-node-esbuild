cd828ac3f2e409bd5f69bee1a3989f2600de965a
Kalebcoding <77423021+Kalebcoding@users.noreply.github.com>
2024-12-15 18:46:07 -0800

    Some code clean up and comments

--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
 const taskVision = require('@mediapipe/tasks-vision')
+const { FaceLandmarker, FilesetResolver, DrawingUtils } = taskVision;
 
-
+// Setting up main variables i will use later
 const audioPlayer: HTMLAudioElement = document.getElementById('face-audio') as HTMLAudioElement;
+let camTracker = null;
+const video: HTMLVideoElement = document.getElementById('webcam') as HTMLVideoElement;
+
 /**
  * Gets audio source with some parameters.
  * 
@@ -9,16 +13,17 @@ const audioPlayer: HTMLAudioElement = document.getElementById('face-audio') as H
  * @param showControls - If we want to show or hide the audio controls
  */
 const setAudioSource = (path: string, showControls: boolean): void => {
-    if (audioPlayer instanceof HTMLAudioElement){
+    if (audioPlayer instanceof HTMLAudioElement) {
         audioPlayer.src = path;
         showControls ? audioPlayer.setAttribute('controls', '') : audioPlayer.removeAttribute("controls");
     }
 }
 
 
-const { FaceLandmarker, FilesetResolver, DrawingUtils } = taskVision;
-let camTracker = null;
-const setFaceLandmarker = async () => {
+/**
+ * Create the Facelandmarker object for us
+ */
+const setFaceLandmarker = async (): Promise<void> => {
     const vision = await FilesetResolver.forVisionTasks(
         "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
     );
@@ -34,34 +39,62 @@ const setFaceLandmarker = async () => {
     })
 };
 
-const video: HTMLVideoElement = document.getElementById('webcam') as HTMLVideoElement;
 
-const setWebcamStream = async() => {
+
+/**
+ * Request media permissions and push the webcam to browser
+ */
+const setWebcamStream = async () => {
     navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
         video.srcObject = stream;
-        video.addEventListener("loadeddata", trackFace);
+        video.addEventListener("loadeddata", trackJaw);
+        video.addEventListener("loadeddata", viewLandmarks);
     });
 }
 
-const trackFace = async() => {
-    let lastVideoTime: = -1;
-        let startTimeMs = performance.now();
-        if (video.currentTime != lastVideoTime) {
-            lastVideoTime = video.currentTime;
-            const detections = camTracker.detectForVideo(video, startTimeMs);
-            const jawOpenVal = detections?.faceBlendshapes[0]?.categories[25].score * 100;
-            if(jawOpenVal > 50 && audioPlayer.paused){
-                audioPlayer.play();
-            }
-
-            if(jawOpenVal < 50 && !audioPlayer.paused){
-                audioPlayer.pause();
-            }
+/**
+ * Logic for playing and stopping the music 
+ * API only exposts jaw open, so we have to use that to track closing / open. 
+ */
+const trackJaw = async () => {
+    let lastVideoTime: Number = -1;
+    let startTimeMs: Number = performance.now();
+    if (video.currentTime != lastVideoTime) {
+        lastVideoTime = video.currentTime;
+        const detections = camTracker.detectForVideo(video, startTimeMs);
+        const jawOpenVal = detections?.faceBlendshapes[0]?.categories[25].score * 100;
+        if (jawOpenVal > 50 && audioPlayer.paused) {
+            audioPlayer.play();
         }
 
-        window.requestAnimationFrame(trackFace);
+        if (jawOpenVal < 50 && !audioPlayer.paused) {
+            audioPlayer.pause();
+        }
+        console.log(`trackjaw Running`);
+    }
+
+    // We can set the response of this to an object to capture id
+    // Then stop it so we dont spam the browser, but thats a different optimization. 
+    window.requestAnimationFrame(trackJaw);
 }
 
+
+/** 
+ * Some test code to see landmarks
+ */
+const viewLandmarks = async () => {
+    let lastVideoTime: Number = -1;
+    let startTimeMs: Number = performance.now();
+    if (video.currentTime != lastVideoTime) {
+        lastVideoTime = video.currentTime;
+        const detections = camTracker.detectForVideo(video, startTimeMs);
+        console.log(`viewLandmarks running`);
+    }
+
+    // We can set the response of this to an object to capture id
+    // Then stop it so we dont spam the browser, but thats a different optimization. 
+    window.requestAnimationFrame(viewLandmarks);
+}
 setAudioSource('./OhHoney.mp3', true);
 setFaceLandmarker();
 setWebcamStream();
